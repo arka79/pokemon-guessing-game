d5b5805c4275eca3bf6b2031a92d2a95011b9b56
Arka Kuilya <72358142+arka79@users.noreply.github.com>
2026-01-03 15:02:25 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -10,7 +10,7 @@ async function loadNewQuestion() {
     });
 
     try {
-        const response = await fetch('https://localhost:3000/api/new-pokemon');
+        const response = await fetch('https://pokemon-backend.onrender.com/api/new-pokemon');
         const data = await response.json();
 
         correctPokemon = data.name;
